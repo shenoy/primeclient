b7b24d7a1c078dd0474f1ee6f9c58a43f8ad8f52
Shenoy <54286073+shenoy@users.noreply.github.com>
2021-01-30 22:13:12 +0000

    Update primeGenerator.js

--- src/primeGenerator.js
+++ src/primeGenerator.js
@@ -2,9 +2,9 @@ function primeGenerator(n) {
   let a = Date.now();
 
   let arr = [];
-  for (i = 2; i <= n; i++) {
-    isPrime = true;
-    for (j = 2; j < parseInt(Math.sqrt(i) + 1); j++) {
+  for (let i = 2; i <= n; i++) {
+    let isPrime = true;
+    for (let j = 2; j < parseInt(Math.sqrt(i) + 1); j++) {
       if (i % j === 0) {
         isPrime = false;
         break;
